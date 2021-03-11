d592953da2c6240ac62a41cd958302e5313d7d7b
Tim Cramer <tstave217@gmail.com>
2021-03-10 22:49:08 -0600

    Adding angular material

---
+++ anubis-family/src/app/app-material.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+  ],
+  exports: [
+    MatToolbarModule,
+    MatIconModule,
+  ]
+})
+export class AppMaterialModule { }
--- anubis-family/src/app/app.module.ts
+++ anubis-family/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppMaterialModule} from './app-material.module';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppMaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
