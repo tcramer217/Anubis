7a3c0acbbe93c85d0dfc0c5df1dced5a8f7ab253
Tim Cramer <tstave217@gmail.com>
2021-03-12 22:57:07 -0600

    minor changes

--- anubis-core/src/test/java/com/tcramer/anubis/core/constants/TaskTypeTest.java
+++ anubis-core/src/test/java/com/tcramer/anubis/core/constants/TaskTypeTest.java
@@ -2,12 +2,10 @@ package com.tcramer.anubis.core.constants;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DisplayName("Verify the functionality of the TaskType.")
 class TaskTypeTest {
