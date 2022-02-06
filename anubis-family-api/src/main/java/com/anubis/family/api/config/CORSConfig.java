99852e7bc34476ea554fe768b96f60e7a12adaaf
Tim Cramer <tstave217@gmail.com>
2022-02-06 17:02:35 -0600

    adding cors config

---
+++ anubis-family-api/src/main/java/com/anubis/family/api/config/CORSConfig.java
@@ -0,0 +1,17 @@
+package com.anubis.family.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CORSConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+        WebMvcConfigurer.super.addCorsMappings(registry);
+    }
+}
